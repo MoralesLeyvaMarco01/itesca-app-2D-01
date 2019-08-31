0b9c6d73f7f902b156b9d6dd432e50ff9636e0ca
MoralesLeyvaMarco01 <Morales.Leyva.Marco.01@gmail.com>
2019-08-31 13:01:43 -0700

    Core assets
    core assets created

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+[SerializeField, Range(-10f,10f)]
+   float movSpeed;
+}
+void Update(){
+    Vector axis= new Vector2(Input.getAxis("Horizontal",Input.getAxis("Vertical")))
+}
